4e40ada5678a3c4c19a6f88f646a195fb59a291f
horvathnauzika <naihorvath@gmail.com>
2024-10-30 15:30:35 +0100

    balra jobbra leptetes

--- src/App.js
+++ src/App.js
@@ -2,9 +2,34 @@ import './App.css';
 import { kepekLista } from './kepek';
 import Jatekter from './components/Jatekter';
 import Nagykartya from './components/Nagykartya';
+import { useState } from 'react';
 
 
 function App() {
+
+  const [kivalasztott, setkivalasztott]=useState(0);
+
+  const kovetkezoKep = () => {
+    setkivalasztott((elozoKep) => {
+        if (elozoKep + 1 < kepekLista.length) {
+            return elozoKep + 1;
+        } else {
+            return 0; // végén visszaáll az első képre
+        }
+    });
+};
+
+  const elozoKep = () => {
+    setkivalasztott((elozoKep) => {
+        if (elozoKep === 0) {
+            return kepekLista.length - 1;
+        } else {
+            return elozoKep - 1;
+        }
+    });
+};
+
+
   return (
     <div className="App">
       <body>
@@ -13,7 +38,7 @@ function App() {
             <h1>Képgaléria</h1>
           </header>
           <article className='row'>
-            <Nagykartya obj={kepekLista[0]}/>
+            <Nagykartya obj={kepekLista[kivalasztott]} kovetkezo={kovetkezoKep} elozo={elozoKep}/>
           </article>
           <article>
             <Jatekter lista={kepekLista}/>
@@ -23,7 +48,6 @@ function App() {
           </footer>
         </main>
       </body>
-
     </div>
   );
 }
--- src/components/Nagykartya.js
+++ src/components/Nagykartya.js
@@ -2,14 +2,14 @@ export default function Nagykartya(props){
     
     return(
         <div className="nagykartya row">
-            <button className="bal col-1">⇦</button>
+            <button className="bal col-1" onClick={props.elozo}>⇦</button>
             <div className="card col-10">
                 <div className="card-body">
                     <h3>{props.obj.cim}</h3>
                     <img src={props.obj.src} alt="kep" className="card-img-top"/>
                 </div>            
             </div>
-            <button className="jobb col-1">⇨</button>
+            <button className="jobb col-1" onClick={props.kovetkezo}>⇨</button>
         </div>
     )
 }
